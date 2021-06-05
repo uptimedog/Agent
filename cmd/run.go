6c3bab703ffb113c1dec7946be65924a88c79b56
Clivern <hello@clivern.com>
2021-06-06 00:08:23 +0200

    add os_release service

--- cmd/run.go
+++ cmd/run.go
@@ -5,39 +5,39 @@
 package cmd
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    log "github.com/sirupsen/logrus"
-    "github.com/spf13/cobra"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 var runCmd = &cobra.Command{
-    Use:   "run",
-    Short: "Start the uptimedog agent",
-    Run: func(cmd *cobra.Command, args []string) {
-        if Verbose {
-            log.SetLevel(log.DebugLevel)
-        }
+	Use:   "run",
+	Short: "Start the uptimedog agent",
+	Run: func(cmd *cobra.Command, args []string) {
+		if Verbose {
+			log.SetLevel(log.DebugLevel)
+		}
 
-        if APIKey == "" {
-            panic("Uptimedog API Key is required")
-        }
+		if APIKey == "" {
+			panic("Uptimedog API Key is required")
+		}
 
-        if APIServer == "" {
-            panic("Uptimedog API Server is required")
-        }
+		if APIServer == "" {
+			panic("Uptimedog API Server is required")
+		}
 
-        log.Info(fmt.Sprintf("Start the agent, Remote server is %s", APIServer))
+		log.Info(fmt.Sprintf("Start the agent, Remote server is %s", APIServer))
 
-        for {
-            log.Info("Agent running")
-            log.Debug("Add -v for verbose logs")
-            time.Sleep(1 * time.Second)
-        }
-    },
+		for {
+			log.Info("Agent running")
+			log.Debug("Add -v for verbose logs")
+			time.Sleep(1 * time.Second)
+		}
+	},
 }
 
 func init() {
-    rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(runCmd)
 }
--- core/service/os_release.go
+++ core/service/os_release.go
@@ -5,94 +5,94 @@
 package service
 
 import (
-    "errors"
-    "os"
+	"errors"
+	"os"
 
-    envparse "github.com/hashicorp/go-envparse"
+	envparse "github.com/hashicorp/go-envparse"
 )
 
 const (
-    // etcOSRelease path
-    etcOSRelease = "/etc/os-release"
-    // usrLibOSRelease path
-    usrLibOSRelease = "/usr/lib/os-release"
+	// etcOSRelease path
+	etcOSRelease = "/etc/os-release"
+	// usrLibOSRelease path
+	usrLibOSRelease = "/usr/lib/os-release"
 )
 
 // OsRelease types
 type OsRelease struct {
-    Name            string
-    ID              string
-    IDLike          string
-    PrettyName      string
-    Variant         string
-    VariantID       string
-    Version         string
-    VersionID       string
-    VersionCodename string
-    BuildID         string
-    ImageID         string
-    ImageVersion    string
+	Name            string
+	ID              string
+	IDLike          string
+	PrettyName      string
+	Variant         string
+	VariantID       string
+	Version         string
+	VersionID       string
+	VersionCodename string
+	BuildID         string
+	ImageID         string
+	ImageVersion    string
 }
 
 // GetOsRelease gets OsRelease instance
 func GetOsRelease(paths []string) (*OsRelease, error) {
 
-    if len(paths) == 0 {
-        paths = []string{etcOSRelease, usrLibOSRelease}
-    }
+	if len(paths) == 0 {
+		paths = []string{etcOSRelease, usrLibOSRelease}
+	}
 
-    release := &OsRelease{}
-    err := release.parse(paths)
+	release := &OsRelease{}
+	err := release.parse(paths)
 
-    return release, err
+	return release, err
 }
 
 // parse parses the OS release file
 func (o *OsRelease) parse(paths []string) error {
 
-    for _, path := range paths {
+	for _, path := range paths {
 
-        if o.Name != "" {
-            return nil
-        }
+		if o.Name != "" {
+			return nil
+		}
 
-        releaseFile, err := os.Open(path)
+		releaseFile, err := os.Open(path)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        defer releaseFile.Close()
+		defer releaseFile.Close()
 
-        _, err = releaseFile.Stat()
+		_, err = releaseFile.Stat()
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        env, err := envparse.Parse(releaseFile)
+		env, err := envparse.Parse(releaseFile)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        o.Name = env["NAME"]
-        o.ID = env["ID"]
-        o.IDLike = env["ID_LIKE"]
-        o.PrettyName = env["PRETTY_NAME"]
-        o.Variant = env["VARIANT"]
-        o.VariantID = env["VARIANT_ID"]
-        o.Version = env["VERSION"]
-        o.VersionID = env["VERSION_ID"]
-        o.VersionCodename = env["VERSION_CODENAME"]
-        o.BuildID = env["BUILD_ID"]
-        o.ImageID = env["IMAGE_ID"]
-        o.ImageVersion = env["IMAGE_VERSION"]
-    }
+		o.Name = env["NAME"]
+		o.ID = env["ID"]
+		o.IDLike = env["ID_LIKE"]
+		o.PrettyName = env["PRETTY_NAME"]
+		o.Variant = env["VARIANT"]
+		o.VariantID = env["VARIANT_ID"]
+		o.Version = env["VERSION"]
+		o.VersionID = env["VERSION_ID"]
+		o.VersionCodename = env["VERSION_CODENAME"]
+		o.BuildID = env["BUILD_ID"]
+		o.ImageID = env["IMAGE_ID"]
+		o.ImageVersion = env["IMAGE_VERSION"]
+	}
 
-    if o.Name == "" {
-        return errors.New("could parse os release files")
-    }
+	if o.Name == "" {
+		return errors.New("could parse os release files")
+	}
 
-    return nil
+	return nil
 }
